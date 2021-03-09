95a19f665e8a03b3aee24f56ae1e7014f147dbf6
ntw-ITM <78532250+ntw-ITM@users.noreply.github.com>
2021-03-09 10:24:23 +0100

    Feature/DGI-20: TokenService WIP depends on DGI-23 (#13)

---
+++ backend/Application/Common/Options/TokenOptions.cs
@@ -0,0 +1,8 @@
+namespace Application.Common.Options
+{
+  public class TokenOptions
+  {
+    public const string Tokens = "Tokens";
+    public string Secret { get; set; }
+  }
+}
---
+++ backend/Web/Services/TokenService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Application.Common.Interfaces;
+using Application.Common.Options;
+using Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+namespace Web.Services
+{
+  public class TokenService : ITokenService
+  {
+    private const double EXPIRE_HOURS = 4.0;
+    private readonly TokenOptions _options;
+    public TokenService(IOptions<TokenOptions> options)
+    {
+      _options = options.Value;
+    }
+    public string CreateToken(User user)
+    {
+      var claims = new List<Claim>();
+      claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+      claims.Add(new Claim(ClaimTypes.Role, user.Role.ToString()));
+      var key = Encoding.ASCII.GetBytes(_options.Secret);
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var descriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.UtcNow.AddHours(EXPIRE_HOURS),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+      };
+      var token = tokenHandler.CreateToken(descriptor);
+      return tokenHandler.WriteToken(token);
+    }
+  }
+}
