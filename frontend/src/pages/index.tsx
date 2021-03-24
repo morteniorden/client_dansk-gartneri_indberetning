addbb7de5c46f310854d74eea1f863f1c4376ac9
Nicolai Wulff <ntw@it-minds.dk>
2021-03-24 15:45:53 +0100

    Create a temporary redirection from the index page to accounts page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,36 @@
 import Demo from "components/Demo/Demo";
+import { AuthContext } from "contexts/AuthContext";
 import { Locale } from "i18n/Locale";
 // import { runTimeTable } from "i18n/runtimeTable";
 import { GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/router";
 import { I18nProps } from "next-rosetta";
+import { useCallback, useContext, useEffect } from "react";
+import { RoleEnum } from "services/backend/nswagts";
 
 const IndexPage: NextPage = () => {
-  return <Demo />;
+  const router = useRouter();
+  const { activeUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    //Todo: Choose what to do with the index page and how to determine what content to display for the user
+    switch (activeUser.role) {
+      case RoleEnum.Admin:
+        router.push("./accounts");
+        break;
+      case RoleEnum.Client:
+        router.push("./accounts");
+        break;
+      case RoleEnum.Accountant:
+        router.push("./accounts");
+        break;
+      default:
+        router.push("./accounts");
+        break;
+    }
+  }, [activeUser, router]);
+
+  return <></>;
 };
 
 export const getStaticProps: GetStaticProps<I18nProps<Locale>> = async context => {
