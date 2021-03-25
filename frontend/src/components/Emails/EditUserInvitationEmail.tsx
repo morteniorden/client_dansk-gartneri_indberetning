42cc288f868a8f1574510db704cd7ffd30a11bc1
Nicolai Wulff <ntw@it-minds.dk>
2021-03-25 10:21:55 +0100

    Create EditEmails next page

--- frontend/src/components/Emails/EditUserInvitationEmail.tsx
+++ frontend/src/components/Emails/EditUserInvitationEmail.tsx
@@ -9,24 +9,24 @@ import {
   Stack,
   Text
 } from "@chakra-ui/react";
-import BasicLayout from "components/Layouts/BasicLayout";
 import { AuthContext } from "contexts/AuthContext";
 import { setAuthToken } from "hooks/useAuth";
 import { useLocales } from "hooks/useLocales";
 import { useRouter } from "next/router";
 import { FC, useCallback, useContext, useEffect, useState } from "react";
 
-import EditUserInvitationEmail from "./EditUserInvitationEmail";
+import EditUserInvitatinoEmailForm from "./EditUserInvitationEmailForm";
 
-const EditEmails: FC = () => {
+const EditUserInvitationEmail: FC = () => {
   const { t } = useLocales();
   const router = useRouter();
   const { activeUser } = useContext(AuthContext);
 
   return (
-    <BasicLayout>
-      <EditUserInvitationEmail />
-    </BasicLayout>
+    <Stack>
+      <Heading>Edit email</Heading>
+      <EditUserInvitatinoEmailForm />
+    </Stack>
   );
 };
-export default EditEmails;
+export default EditUserInvitationEmail;
