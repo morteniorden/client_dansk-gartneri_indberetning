54c0a280c9002f93bcc6fa3f51da78a9be8653f5
Nicolai Wulff <ntw@it-minds.dk>
2021-03-26 17:15:47 +0100

    Experiment with adding CKEditor, and I got it to work.

--- frontend/src/components/Emails/EditUserInvitationEmail.tsx
+++ frontend/src/components/Emails/EditUserInvitationEmail.tsx
@@ -1,32 +1,23 @@
-import {
-  Button,
-  Flex,
-  Grid,
-  Heading,
-  HStack,
-  IconButton,
-  Input,
-  Stack,
-  Text
-} from "@chakra-ui/react";
+import { Heading, Stack } from "@chakra-ui/react";
 import { AuthContext } from "contexts/AuthContext";
-import { setAuthToken } from "hooks/useAuth";
 import { useLocales } from "hooks/useLocales";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
-import { FC, useCallback, useContext, useEffect, useState } from "react";
-
-import EditUserInvitatinoEmailForm from "./EditUserInvitationEmailForm";
+import { FC, useContext } from "react";
 
 const EditUserInvitationEmail: FC = () => {
   const { t } = useLocales();
   const router = useRouter();
   const { activeUser } = useContext(AuthContext);
 
+  const Editor = dynamic(() => import("./Editor"), { ssr: false });
+
   return (
     <Stack>
       <Heading>Edit email</Heading>
-      <EditUserInvitatinoEmailForm />
+      <Editor />
     </Stack>
   );
 };
 export default EditUserInvitationEmail;
+//<EditUserInvitatinoEmailForm />
