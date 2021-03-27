7fc44dd4231a9278649b667f98cd3c3597c6538d
Nicolai Wulff <ntw@it-minds.dk>
2021-03-27 16:01:53 +0100

    Add tooltips to inputs on extended editor

--- frontend/src/components/Emails/Editor/ExtendedMailEditor.tsx
+++ frontend/src/components/Emails/Editor/ExtendedMailEditor.tsx
@@ -1,4 +1,12 @@
-import { FormControl, FormLabel, Input, InputGroup, InputLeftAddon, Stack } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Stack,
+  Tooltip
+} from "@chakra-ui/react";
 import { useLocales } from "hooks/useLocales";
 import dynamic from "next/dynamic";
 import { FC } from "react";
@@ -22,36 +30,51 @@ const ExtendedMailEditor: FC<Props> = ({ state, setState, variant }) => {
 
   return (
     <Stack>
-      <FormControl id="name">
-        <InputGroup>
-          <InputLeftAddon w="70px">Navn</InputLeftAddon>
-          <Input
-            maxW="max-content"
-            minW="300px"
-            value={state.name}
-            onChange={e => setState({ ...state, ...{ name: e.target.value } })}></Input>
-        </InputGroup>
-      </FormControl>
-      <FormControl id="title">
-        <InputGroup>
-          <InputLeftAddon w="70px">Titel</InputLeftAddon>
-          <Input
-            maxW="max-content"
-            minW="300px"
-            value={state.title}
-            onChange={e => setState({ ...state, ...{ title: e.target.value } })}></Input>
-        </InputGroup>
-      </FormControl>
-      {variant == "endCTAButton" && (
-        <FormControl id="ctaButton">
+      <Tooltip
+        label="Navn på email. Vises ikke for modtageren."
+        aria-label="A tooltip"
+        placement="auto-end">
+        <FormControl id="name">
           <InputGroup>
-            <InputLeftAddon w="70px">{t("mailEditor.ctaButtonInputLabel")}</InputLeftAddon>
+            <InputLeftAddon w="70px">Navn</InputLeftAddon>
             <Input
-              maxW="300px"
-              value={state.ctaButtonText}
-              onChange={e => setState({ ...state, ...{ ctaButton: e.target.value } })}></Input>
+              maxW="max-content"
+              minW="300px"
+              value={state.name}
+              onChange={e => setState({ ...state, ...{ name: e.target.value } })}></Input>{" "}
           </InputGroup>
         </FormControl>
+      </Tooltip>
+      <Tooltip
+        label="Titel på email, som den vises for modtageren."
+        aria-label="A tooltip"
+        placement="auto-end">
+        <FormControl id="title">
+          <InputGroup>
+            <InputLeftAddon w="70px">Titel</InputLeftAddon>
+            <Input
+              maxW="max-content"
+              minW="300px"
+              value={state.title}
+              onChange={e => setState({ ...state, ...{ title: e.target.value } })}></Input>
+          </InputGroup>
+        </FormControl>
+      </Tooltip>
+      {variant == "endCTAButton" && (
+        <Tooltip
+          label="Tekst på knappen, der vises nederst i mailen."
+          aria-label="A tooltip"
+          placement="auto-end">
+          <FormControl id="ctaButton">
+            <InputGroup>
+              <InputLeftAddon w="70px">{t("mailEditor.ctaButtonInputLabel")}</InputLeftAddon>
+              <Input
+                maxW="300px"
+                value={state.ctaButtonText}
+                onChange={e => setState({ ...state, ...{ ctaButton: e.target.value } })}></Input>
+            </InputGroup>
+          </FormControl>
+        </Tooltip>
       )}
       <Editor
         content={state.htmlContent}
