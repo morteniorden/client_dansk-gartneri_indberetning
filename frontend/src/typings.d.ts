d17d6b34c4a2220671c84efabc077bf8c7a5b6d8
Nicolai Wulff <ntw@it-minds.dk>
2021-03-29 16:08:31 +0200

    Try another module declaration

--- frontend/src/typings.d.ts
+++ frontend/src/typings.d.ts
@@ -1,2 +1,2 @@
 declare module "@ckeditor/ckeditor5-react";
-declare module "src/components/Emails/Editor/CKEditorBuild/ckeditor5/build/ckeditor.js";
+declare module "src/components/Emails/Editor/CKEditorBuild/ckeditor5/build/ckeditor";
