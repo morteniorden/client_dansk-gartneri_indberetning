c8be157a41b061502a04789994de4584c89d8d61
Michael Jeppesen <mjp@it-minds.dk>
2022-01-05 11:45:48 +0100

    Added hook to format numbers with thousand seperators, and convert back

---
+++ frontend/src/hooks/useFormatNumber.ts
@@ -0,0 +1,46 @@
+import { useCallback } from "react";
+
+import { useLocales } from "./useLocales";
+
+export const useFormatNumber = () => {
+  const { formatCurrency } = useLocales();
+
+  const formatNumberThousandSep = useCallback(
+    (formatValue: string | number, leftOrRight: "left" | "right") => {
+      if (formatValue == "") return 0;
+      let thousandSep = ".";
+      let decimalSep = ",";
+      if (leftOrRight == "left") {
+        thousandSep = ",";
+        decimalSep = ".";
+      }
+      if (typeof formatValue == typeof 1) formatValue = (formatValue as number).toString();
+      formatValue = formatValue.toString().replaceAll(thousandSep, "");
+      formatValue = parseInt(formatValue.split(decimalSep)[0]);
+      formatValue = Math.max(0, Math.min(1000000000, formatValue));
+      const retVal = formatCurrency(formatValue).numberPartsOnly.split(decimalSep)[0];
+      return retVal == "NaN" ? 0 : retVal;
+    },
+    [formatCurrency]
+  );
+
+  const parseFormat = useCallback((formatValue: string | number, leftOrRight: "left" | "right") => {
+    if (formatValue == "") return 0;
+    let thousandSep = ".";
+    let decimalSep = ",";
+    if (leftOrRight == "left") {
+      thousandSep = ",";
+      decimalSep = ".";
+    }
+    if (typeof formatValue == typeof 1) formatValue = (formatValue as number).toString();
+    formatValue = formatValue.toString().replaceAll(thousandSep, "");
+    formatValue = parseInt(formatValue.split(decimalSep)[0]);
+    formatValue = Math.max(0, Math.min(1000000000, formatValue));
+    return formatValue;
+  }, []);
+
+  return {
+    formatNumberThousandSep,
+    parseFormat
+  };
+};
