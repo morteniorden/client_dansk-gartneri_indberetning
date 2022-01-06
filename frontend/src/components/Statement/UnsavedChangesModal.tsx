c702d1d0d233d62dd915931348466b888fb79af0
Michael Jeppesen <mjp@it-minds.dk>
2022-01-06 09:21:20 +0100

    Made modal for when there is unsaved changes

---
+++ frontend/src/components/Statement/UnsavedChangesModal.tsx
@@ -0,0 +1,58 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text
+} from "@chakra-ui/react";
+import { EditStatementContext } from "contexts/EditStatementContext";
+import { useRouter } from "next/router";
+import React, { FC, useContext, useState } from "react";
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const UnsavedChangesModal: FC<Props> = ({ isOpen, onClose }) => {
+  const { save } = useContext(EditStatementContext);
+  const router = useRouter();
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Unsaved changes</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>You have unsaved changes.</Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              m={1}
+              colorScheme="green"
+              onClick={() => {
+                save();
+                router.push("/mystatements");
+              }}>
+              Save
+            </Button>
+            <Button m={1} colorScheme="yellow" onClick={onClose}>
+              Close
+            </Button>
+            <Button m={1} colorScheme="red" onClick={() => router.push("/mystatements")}>
+              Ignore
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default UnsavedChangesModal;
