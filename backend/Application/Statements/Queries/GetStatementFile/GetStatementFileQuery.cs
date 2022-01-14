9615e04fd30274af093b0313bc1aaf8e931c4319
Michael Jeppesen <mjp@it-minds.dk>
2022-01-14 15:23:03 +0100

    Added query to get the file for a statement

---
+++ backend/Application/Statements/Queries/GetStatementFile/GetStatementFileQuery.cs
@@ -0,0 +1,64 @@
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Options;
+using Application.Common.Security;
+using Domain.Entities;
+using Domain.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Application.Statements.Queries.GetStatementFile
+{
+  [Authorize(Role = RoleEnum.Admin)]
+  public class GetStatementFileQuery : IRequest<StatementFileDto>
+  {
+    public int StatementId { get; set; }
+    public class GetStatementFileQueryHandler : IRequestHandler<GetStatementFileQuery, StatementFileDto>
+    {
+      private readonly IApplicationDbContext _context;
+      private readonly FileDriveOptions _options;
+
+      public GetStatementFileQueryHandler(IApplicationDbContext context, IOptions<FileDriveOptions> options)
+      {
+        _context = context;
+        _options = options.Value;
+      }
+
+      public async Task<StatementFileDto> Handle(GetStatementFileQuery request, CancellationToken cancellationToken)
+      {
+        Statement statement = await _context.Statements.FirstAsync(e => e.Id == request.StatementId, cancellationToken);
+
+        if (statement == null)
+        {
+          throw new NotFoundException(nameof(Statement), request.StatementId);
+        }
+
+        string file = Directory.EnumerateFiles(_options.ConsentPath, statement.StatementFileName)
+          .FirstOrDefault();
+
+        if (file == null)
+        {
+          throw new NotFoundException("Statement file for statement with id " + statement.Id + " was not found.");
+        }
+
+        StatementFileDto result = new StatementFileDto
+        {
+          FileName = statement.StatementFileName
+        };
+
+        using (Stream fileStream = new FileStream(file, FileMode.Open))
+        {
+          result.Data = new byte[fileStream.Length];
+          await fileStream.ReadAsync(result.Data, cancellationToken);
+        }
+
+        return result;
+      }
+    }
+  }
+}
---
+++ backend/Application/Statements/StatementFileDto.cs
@@ -0,0 +1,9 @@
+
+namespace Application.Statements
+{
+  public class StatementFileDto
+  {
+    public string FileName { get; set; }
+    public byte[] Data { get; set; }
+  }
+}
