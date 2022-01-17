ee4d5b09c0ed9bd788eb1051e1b5a68ad047cfe0
Michael Jeppesen <mjp@it-minds.dk>
2022-01-17 15:43:50 +0100

    Added a callback command which signs off a statement as an accountant

--- frontend/src/services/backend/nswagts.ts
+++ frontend/src/services/backend/nswagts.ts
@@ -499,6 +499,7 @@ export interface IStatementClient {
     getStatementsCSV(accountingYear?: number | null | undefined): Promise<CSVResponseDto>;
     unassignAccountant(id: number): Promise<FileResponse>;
     consentToStatement(id: number, file?: FileParameter | null | undefined): Promise<GetSigningUrlDto>;
+    consentCallback(id: number): Promise<FileResponse>;
     getAllStatementInfo(): Promise<StatementInfoDto[]>;
     updateStatementInfo(year: number, command: UpdateStatementInfoCommand): Promise<Unit>;
     getConsentFile(statementId?: number | undefined): Promise<ConsentFileDto>;
@@ -954,6 +955,43 @@ export class StatementClient extends ClientBase implements IStatementClient {
             });
         }
         return Promise.resolve<GetSigningUrlDto>(<any>null);
+    }
+
+    consentCallback(id: number): Promise<FileResponse> {
+        let url_ = this.baseUrl + "/api/Statement/{id}/consent/callback";
+        if (id === undefined || id === null)
+            throw new Error("The parameter 'id' must be defined.");
+        url_ = url_.replace("{id}", encodeURIComponent("" + id));
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ = <RequestInit>{
+            method: "PUT",
+            headers: {
+                "Accept": "application/octet-stream"
+            }
+        };
+
+        return this.transformOptions(options_).then(transformedOptions_ => {
+            return this.http.fetch(url_, transformedOptions_);
+        }).then((_response: Response) => {
+            return this.transformResult(url_, _response, (_response: Response) => this.processConsentCallback(_response));
+        });
+    }
+
+    protected processConsentCallback(response: Response): Promise<FileResponse> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200 || status === 206) {
+            const contentDisposition = response.headers ? response.headers.get("content-disposition") : undefined;
+            const fileNameMatch = contentDisposition ? /filename="?([^"]*?)"?(;|$)/g.exec(contentDisposition) : undefined;
+            const fileName = fileNameMatch && fileNameMatch.length > 1 ? fileNameMatch[1] : undefined;
+            return response.blob().then(blob => { return { fileName: fileName, data: blob, status: status, headers: _headers }; });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<FileResponse>(<any>null);
     }
 
     getAllStatementInfo(): Promise<StatementInfoDto[]> {
